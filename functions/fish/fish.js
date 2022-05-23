60d1f691d3db40cccf6526ed1f648e6a51b668fd
Romain Colombat <romain.colombat@my-mooc.com>
2022-05-23 10:37:47 +0200

    Refacto Netlify project

--- functions/fish/fish.js
+++ functions/fish/fish.js
@@ -2,18 +2,9 @@ import {filterFish, fishData} from "./utils.js";
 
 exports.handler = async function (event, context) {
     const input = event.queryStringParameters.q;
-    if(!input) {
-        return {
-            statusCode: 404,
-            body: JSON.stringify(fishData),
-            headers: {
-                "access-control-allow-origin": "*",
-            },
-        };
-    }
     return {
         statusCode: 200,
-        body: JSON.stringify(filterFish(input)),
+        body: JSON.stringify(input ? filterFish(input) : fishData),
         headers: {
             "access-control-allow-origin": "*",
         },
